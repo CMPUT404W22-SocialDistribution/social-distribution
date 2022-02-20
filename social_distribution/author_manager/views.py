caef14331a8981c624e3e8e1109d21fcf878307b
Hang Ngo <thngo@ualberta.ca>
2022-02-19 23:37:57 -0700

    add author api

--- social_distribution/author_manager/views.py
+++ social_distribution/author_manager/views.py
@@ -5,6 +5,11 @@ from .models import Author
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import ProfileSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 def sign_up(request):
     if request.method == 'POST':
@@ -38,3 +43,34 @@ def sign_in(request):
 def home(request):
     # this is temp we should probably do AJAX later
     return render(request, 'author_manager/index.html')
+
+
+class ProfileAPI(APIView):
+    """
+    An API endpoint allows viewing and updating a profile.
+    ...
+    Attributes:
+    Methods:
+        GET:
+            Retrieve an author's profile.
+        PUT:
+            Update an author's profile.
+    """
+    def get(self, request, id):
+        """
+        Handling GET request. Showing the author's profile.
+        Returns:
+            - If successful:
+                Status 200 and the author's basic information.
+        """
+        profile = Author.objects.get(id=id)
+        # profile = get_object_or_404(Author, id=id)
+        serializer = ProfileSerializer(profile, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class GetAllAuthors(APIView):
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = ProfileSerializer(authors, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
