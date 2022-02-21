b9fd515ac5c2aed32c0fdba045267fd4fe75de1d
Hang Ngo <thngo@ualberta.ca>
2022-02-20 22:26:45 -0700

    added authors api, sidebar, navbar

--- social_distribution/author_manager/serializers.py
+++ social_distribution/author_manager/serializers.py
@@ -5,5 +5,12 @@ from .models import Author
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = '__all__'
-        
+        fields = ['user','uuid', 'type', 'id', 'host', 'displayName', 'url', 'github', 'profileImage']
+
+    def __init__(self, *args, **kwargs):
+        remove_fields = kwargs.pop('remove_fields', None)
+        super(ProfileSerializer, self).__init__(*args, **kwargs)
+
+        if remove_fields:
+            for field in remove_fields:
+                self.fields.pop(field)
