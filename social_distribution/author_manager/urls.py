628044f5fd4d40b3c30f88948a9dc1a35869960d
hellosusan <ngocthao@ualberta.ca>
2022-02-23 05:36:16 -0700

    add friends and inbox template

--- social_distribution/author_manager/urls.py
+++ social_distribution/author_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import ProfileAPI, GetAllAuthors
+from .views import *
+
 app_name = 'author_manager'
 urlpatterns = [
     path('signup/', views.sign_up, name ="sign_up"),
@@ -11,4 +12,6 @@ urlpatterns = [
     path('authors/<str:id>/',  ProfileAPI.as_view(), name='profile'),
     path('authors/', GetAllAuthors.as_view(), name='getAllAuthors'),
     path('authors/<str:id>/edit', views.profile_edit, name='editProfile'),
+    path('authors/<str:author_id>/friends', friends_view, name ="friends"),
+    path('authors/<str:author_id>/inbox', inbox_view, name = "inbox"),
 ]
