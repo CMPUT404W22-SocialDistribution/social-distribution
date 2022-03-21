d9b661a0affd7b9a39524e3781ac2f8bbab09fd2
hellosusan <ngocthao@ualberta.ca>
2022-03-21 13:16:40 -0600

    add tests for API inbox

--- social_distribution/author_manager/tests/test_APIs.py
+++ social_distribution/author_manager/tests/test_APIs.py
@@ -1,9 +1,12 @@
 
+from contextlib import nullcontext
+from urllib import response
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from author_manager.models import * 
+from posts.models import *
 from base64 import b64encode
 
 
@@ -192,8 +195,13 @@ class InboxTest(APITestCase):
         user1 = User.objects.create_user(username="test1", password="password1", is_active=True)
         self.author1 = Author.objects.create(user=user1)
 
-        # create posts to send to inbox
-        self.follow = FriendRequest(actor=self.author1, object=self.author)
+        # create items to send to inbox
+        self.follow = FriendRequest.objects.create(actor=self.author1, object=self.author)
+        self.post = Post.objects.create(author=self.author1, title='Send To Inbox',
+                                                      content='Test send to in box', content_type='text/plain', 
+                                                      visibility='private', visibleTo=self.username)
+        self.comment = Comment.objects.create(author=self.author1, comment='Test send to inbox',
+                                                contentType= 'text/plain', post=self.post )
         
 
         # login user account
@@ -208,4 +216,45 @@ class InboxTest(APITestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-       
+    def test_post_send_follow_to_inbox(self):  
+        request_body = {'item': {'type': 'follow', 'id': self.follow.id}}
+        response = self.client.post(
+                    self.url,
+                    request_body,
+                    format='json'
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_send_post_to_inbox(self):  
+        request_body = {'item': {'type': 'post', 'id': self.post.id}}
+        response = self.client.post(
+                    self.url,
+                    request_body,
+                    format='json'
+        )
+        self.assertEqual(response.status_code, 200)
+    
+    def test_post_send_comment_to_inbox(self):  
+        request_body = {'item': {'type': 'comment', 'id': self.comment.id}}
+        response = self.client.post(
+                    self.url,
+                    request_body,
+                    format='json'
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_create_send_like_to_inbox(self):  
+        request_body = {'item': 
+                            {'type': 'like', 
+                            'summary': 'test1 likes your post', 
+                            'author': self.author.id, 
+                            'post': self.post.id, 
+                            'comment': ''
+                            }
+                        }
+        response = self.client.post(
+                    self.url,
+                    request_body,
+                    format='json'
+        )
+        self.assertEqual(response.status_code, 200)
