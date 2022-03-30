90d5798c06cfdc546285131e30c8c1a92baf7c0e
Nguyen Vu <nhvu@ualberta.ca>
2022-03-30 15:58:45 -0600

    Fix remote post from squawker 404

--- social_distribution/posts/views.py
+++ social_distribution/posts/views.py
@@ -503,7 +503,7 @@ def post_detail(request, author_id, post_id):
                         "post": post,
                         "comments": data["commentsSrc"]["comments"]
                     }
-            elif node_url == 'http://squawker-cmput404.herokuapp.com/':
+            elif node_url == 'http://squawker-cmput404.herokuapp.com/' or node_url == 'https://squawker-cmput404.herokuapp.com/':
                 posts_url = f"{node_url}api/authors/{author_id}/posts/{post_id}"
                 response = requests.get(posts_url, headers=HEADERS, auth=("squawker", "cmput404"))
                 if response.status_code == 200:
